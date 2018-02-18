59057af57042efe32d6ff8a3b326860884c2a621
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2018-02-18 02:43:30 +0000

    - Added provides as suggested by CPANTS.

--- lib/WWW/SmartSheet.pm
+++ lib/WWW/SmartSheet.pm
@@ -1,6 +1,6 @@
 package WWW::SmartSheet;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Moo;
 use MooX::late;
--- lib/WWW/SmartSheet/Sheet.pm
+++ lib/WWW/SmartSheet/Sheet.pm
@@ -1,6 +1,6 @@
 package WWW::SmartSheet::Sheet;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Moo;
 use MooX::late;
